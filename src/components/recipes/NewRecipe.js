fe0748a662b1fb16913d6502c3ab8b9a203a92cb
Stacy Griffy <sgriffy2@gmail.com>
2024-01-31 17:15:32 -0600

    New Post view and functionality

--- src/components/recipes/NewRecipe.js
+++ src/components/recipes/NewRecipe.js
@@ -1,12 +1,70 @@
+import { useEffect, useState } from "react";
 import { RecipeForm } from "../forms/RecipeForm";
+import { addNewRecipe } from "../../services/recipeService";
+import { useNavigate } from "react-router-dom";
 
-export const NewRecipe = () => {
-  const handleInputChange = (e) => {};
+export const NewRecipe = ({ currentUser }) => {
+  const [newRecipe, setNewRecipe] = useState({
+    title: "",
+    description: "",
+    ingredients: "",
+    steps: "",
+    categoryId: 0,
+    mealId: 0,
+  });
+  const [stepsString, setStepsString] = useState("");
+  const [ingredientsString, setIngredientsString] = useState("");
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const stepsString = newRecipe.steps;
+    const updatedSteps = stepsString.split("\n").join(", ");
+    setStepsString(updatedSteps);
+
+    const ingredientsString = newRecipe.ingredients;
+    const updatedIngredients = ingredientsString.split("\n").join(", ");
+    setIngredientsString(updatedIngredients);
+  }, [newRecipe]);
+
+  const handleInputChange = (e) => {
+    const stateCopy = { ...newRecipe };
+    if (e.target.name === "categoryId" || e.target.name === "mealId") {
+      stateCopy[e.target.name] = parseInt(e.target.value);
+      setNewRecipe(stateCopy);
+    } else {
+      stateCopy[e.target.name] = e.target.value;
+      setNewRecipe(stateCopy);
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const recipeObj = {
+      title: newRecipe.title,
+      description: newRecipe.description,
+      ingredients: ingredientsString,
+      userId: currentUser.id,
+      steps: stepsString,
+      categoryId: newRecipe.categoryId,
+      mealId: newRecipe.mealId,
+      date: new Date(),
+    };
+
+    addNewRecipe(recipeObj).then((res) => {
+      navigate(`/recipes/${res.id}`);
+    });
+  };
 
   return (
     <div>
       <h2>New Recipe</h2>
-      <RecipeForm handleInputChange={handleInputChange} />
+      <RecipeForm
+        handleInputChange={handleInputChange}
+        handleSubmit={handleSubmit}
+        newRecipe={newRecipe}
+      />
     </div>
   );
 };
