07934f3c8193a354e54d7da6d9454695620db176
Stacy Griffy <sgriffy2@gmail.com>
2024-02-02 11:16:34 -0600

    Profile edit form

--- src/services/userService.js
+++ src/services/userService.js
@@ -19,3 +19,13 @@ export const createUser = (customer) => {
     body: JSON.stringify(customer),
   }).then((res) => res.json());
 };
+
+export const editUser = (user) => {
+  return fetch(`http://localhost:8088/users/${user.id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  });
+};
